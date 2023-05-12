14aef67be66c4e9c264fd31214967e06e85d4029
Joshua Rubin <jgrubin@gmail.com>
2023-05-12 05:09:52 -0700

    Updated activity.  Lots of work on analyzer.

--- pages/1 Simulated GenAI Applicaiton.py
+++ pages/1 Simulated GenAI Applicaiton.py
@@ -8,6 +8,9 @@ import streamlit as st
 from uuid import uuid1
 from decimal import Decimal
 
+SESSION_ID = 'banana'
+TOT_PROMPTS_TO_DO = 15
+
 OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
 AWS_S3_BUCKET_NAME = st.secrets['AWS_S3_BUCKET_NAME']
 AWS_DYNAMODB_TABLE_NAME = st.secrets['AWS_DYNAMODB_TABLE_NAME']
@@ -43,7 +46,7 @@ def get_embedding(prompt='a gray cat jumping between pieces of furnatiure', mode
     response = openai.Embedding.create(
       api_key=OPENAI_API_KEY,
       input=[prompt],
-      model=model        
+      model=model
     )
 
     return [Decimal(str(x)[:9]) for x in response['data'][0]['embedding']]
@@ -56,55 +59,90 @@ def submit_data():
     data_dict[KEY_USER_ID] = data_dict[KEY_USER_ID].lower()
 
     s3_bucket.upload_fileobj(io.BytesIO(data_dict.pop(KEY_IMAGE)), data_dict[KEY_UUID] + '.png', )
-    print('image written to s3')
+    # print('image written to s3')
 
     ddb_table.put_item(Item=data_dict)
-    print('ddb record written')
-    print(data_dict)
+    # print('ddb record written')
+    # print(data_dict)
+
+
+    state[KEY_PROMPT_NUMBER] += 1
 
     st.balloons()
 
     next_prompt()
 
 
+politics_details = {'Who': ['the president', 'a mayer', 'a senator', 'a politician', 'protestors'],
+                    'Where': ['the White House', 'a restaurant', 'a podium', 'a park'],
+                    'What': ['a scandal', 'an agreement', 'an election', 'a debate', 'a surprise', 'a celebration']}
+
+business_details = {'Who': ['an executive', 'a mechanic', 'a family', 'workers'],
+                    'Where': ['Wall Street', 'a grocery store', 'a shipping container', 'a factory',
+                              'a corporate headquarters'],
+                    'What': ['an infographic', 'a protest', 'inflation', 'the banking sector', 'corn futures']}
+
+arts_details = {'Who': ['a painter', 'musicians', 'a photographer', 'a child'],
+                'Where': ['a gallery', 'a cafe', 'a museum', 'a natural scene'],
+                'What': ['a painting', 'a sculpture', 'a performance', 'a paintbrush', 'a sweater', 'a portrait']}
+
+sports_details = {'Game': ['golf', 'soccer', 'baseball', 'cricket', 'football', 'climbing', 'cycling'],
+                  'What': ['victory', 'rivalry', 'history', 'weather', 'new record', 'injury'],
+                  'Where': ['stadium', 'university', 'back yard', 'arena', 'national park']}
+
+travel_details = {'Where': ['a hotel', 'an airplane', 'city streets', 'a hiking trail', 'a beach on an island'],
+                  'What': ['a map', 'noodles', 'luxury', 'delight', 'delay', 'wildlife'],
+                  'Who': ['a family', 'a monkey', 'a tour guide', 'a pilot', 'a mountaineer']}
+
+funny_details = {'Who': ['a cat', 'some kids', 'a family', 'some people in an office', 'a clown'],
+                 'What': ['pizza', 'ice cream', 'doing homework', 'on a date', 'watching a movie', 'dancing',
+                          'a laptop', 'in a treehouse'],
+                 'When': ['the stone age', 'the future', '1950s', 'during the pandemic']}
+
+categories_details = {'politics': politics_details,
+                      'business': business_details,
+                      'arts': arts_details,
+                      'sports': sports_details,
+                      'travel': travel_details,
+                      'cartoon': funny_details}
+
+categories_examples = {
+    'politics': ["A photo of the president embarrassed in front of reporters during the correspondents' dinner.",
+                 "A black and white photo of a politician with a microphone."],
+    'business': ['An graph showing recent dramatic changes in stock prices.',
+                 'A photo of factory workers busy building cars in a factory.'],
+    'arts': ['A photo of a woman painting a colorful canvas with a large brush.',
+             "A painting of a dramatic scene on stage with a person grabbing another by shoulder."],
+    'sports': ['A black and white photo of a boxer with hands stretched over their head in victory.',
+               'A close-up photo of soccer teammates hugging in the rain after a difficult victory.'],
+    'travel': ["An illustration of a map showing a ship's coarse between tropical islands.",
+               'A photo of a beautiful hotel and trees and a long driveway in the mountains.'],
+    'cartoon': ['An illustration of a clown mowing the lawn.',
+              'A cartoon of a horse working on a laptop.']}
 
 
+def generate_clues(prompt_number=0):
+    categories = list(categories_details)
+    category = random.choice(categories)
 
-categories = {'places': ['Park', 'A space station', 'Gym', 'Museum', 'Library', 'Coffee Shop',
-                        'The planet Mercury', 'Vinyard', 'Country Road', 'Tokyo', 'Paris',
-                        'Dubai', 'Mogadishu', 'Vancouver'],
-              'activities': ['Playing an instrument', 'Hiking', 'Knitting',
-                            'Cooking','Skydiving', 'Reading', 'Skydiving',
-                            'Surfing', 'Painting ', 'Playing a sport', 'Writing'],
-              'people': ['Painter', 'Musician', 'Doctor', 'Engineer', 'Explorer', 'Soldier', 'Explorer',
-                        'Dancer', 'Veterinarian', 'Sculptor', 'Mechanic', 'Librarian'],
-              'animals': ['Dog', 'Cat', 'Lizard', 'Bear', 'Snake', 'Insect', 'Elephant','Zebra'],
-              'things': ['Tree', 'Tomato', 'Laptop', 'TV', 'Rocket', 'Time Machine', 'Wallet', 'Banana',
-                        'Hammer', 'Playing cards', 'Dessert', 'Old shoe', 'Crown', 'Fire', 'Sand', 'Snowball']
+    cat_cats = list(categories_details[category].keys())
+    random.shuffle(cat_cats)
 
-             }
+    clue0 = random.choice(categories_details[category][cat_cats[0]])
+    clue1 = random.choice(categories_details[category][cat_cats[1]])
 
-def generate_clues():
-
-    n = random.choice([2,  3,])
-
-    cat_keys = list(categories.keys())
-    random.shuffle(cat_keys)
-
-    final_categories = [cat_keys.pop() for x in range(n)]
-
-    return {cat: random.choice(categories[cat]) for cat in final_categories}
+    return category, {cat_cats[0]: clue0, cat_cats[1]: clue1}
 
 
 EMPTY = ''
 
 KEY_USER_ID = 'user'
 KEY_PROMPT = 'prompt'
-KEY_CLUE = 'clue'
 KEY_IMAGE = 'image'
 KEY_EMBEDDING = 'embedding'
 KEY_TIME = 'time'
 KEY_HUMAN_TIME = 'human_time'
+KEY_PROMPT_NUMBER = 'prompt_number'
 
 KEY_UUID = 'prompt_id'
 KEY_FEEDBACK_QUALITY = 'feedback_quality'
@@ -112,10 +150,13 @@ KEY_FEEDBACK_FIDELITY = 'feedback_fidelity'
 KEY_FEEDBACK_DISTORTION = 'feedback_distortion'
 KEY_FEEDBACK_BIAS = 'feedback_bias'
 KEY_FEEDBACK_NOTES = 'feedback_notes'
+KEY_SESSION_ID = 'session_id'
+KEY_CATEGORY = 'category'
+KEY_FEATURES = 'features'
 
-STATE_KEYS = [KEY_USER_ID, KEY_PROMPT, KEY_CLUE, KEY_IMAGE, KEY_EMBEDDING, KEY_TIME, KEY_HUMAN_TIME, KEY_UUID,
+STATE_KEYS = [KEY_USER_ID, KEY_PROMPT, KEY_IMAGE, KEY_EMBEDDING, KEY_TIME, KEY_HUMAN_TIME, KEY_UUID,
               KEY_FEEDBACK_QUALITY, KEY_FEEDBACK_FIDELITY, KEY_FEEDBACK_DISTORTION, KEY_FEEDBACK_BIAS,
-              KEY_FEEDBACK_NOTES]
+              KEY_FEEDBACK_NOTES, KEY_SESSION_ID, KEY_PROMPT_NUMBER, KEY_CATEGORY, KEY_FEATURES]
 
 state = st.session_state
 
@@ -123,11 +164,15 @@ for k in STATE_KEYS:
     if k not in state:
         state[k] = EMPTY
 
+if state[KEY_PROMPT_NUMBER] == EMPTY:
+    state[KEY_PROMPT_NUMBER] = 0
+
+state[KEY_SESSION_ID] = SESSION_ID
+
 
 def next_prompt():
     state[KEY_PROMPT] = EMPTY
-    state[KEY_IMAGE] = EMPTY
-    state[KEY_CLUE] = generate_clues()
+    state[KEY_CATEGORY], state[KEY_FEATURES] = generate_clues()
 
 
 def reset_results():
@@ -135,51 +180,62 @@ def reset_results():
     state[KEY_EMBEDDING] = EMPTY
     state[KEY_TIME] = EMPTY
 
-
 st.header("Part 1: Simulating a Generative AI Workflow")
-st.subheader("Let's generate some data!")
 
-st.markdown("The purpose of Part I is to generate a collection of data from a generative AI task.  "
-            "Imagine that you’re responsible for generating thumbnail images for a newspaper or magazine"
-            " based on some features you’ve been given.\n"
-            "1. You’ll be presented with a set of keywords.\n"
-            "2. Use your imagination to turn them into a prompt for a generative image model.\n"
-            "3. Look at the image generated and answer a few questions about how well the model performed.\n"
-            "4. Click “Submit” and we’ll store all this info for subsequent analysis.\n")
+st.text(f'Session:  {state[KEY_SESSION_ID]}')
 
-st.text_input("First, enter your name or an email address. (This is so you can identify data you generated – we won't "
-              "add you to any mailing lists without your permission.)", key=KEY_USER_ID)
+
+
+# st.subheader("Let's generate some data!")
+
+# st.markdown("The purpose of Part I is to generate a collection of data from a generative AI task.  "
+#             "Imagine that you’re responsible for generating thumbnail images for a newspaper or magazine"
+#             " based on some features you’ve been given.\n"
+#             "1. You’ll be presented with a set of keywords.\n"
+#             "2. Use your imagination to turn them into a prompt for a generative image model.\n"
+#             "3. Look at the image generated and answer a few questions about how well the model performed.\n"
+#             "4. Click “Submit” and we’ll store all this info for subsequent analysis.\n")
+#
+
+text_input_container = st.empty()
+text_input_container.text_input("First, enter your name or an email address. (This is so you can identify data you generated – we won't "
+                                   "add you to any mailing lists without your permission.)", key=KEY_USER_ID)
 
 if state[KEY_USER_ID] == EMPTY:
     st.stop()
 
-st.write(f"Fabulous, {state[KEY_USER_ID]}")
+text_input_container.empty()
+st.text(f"User:     {state[KEY_USER_ID]}")
 
-st.markdown('---')
+st.text(f'Prompt #: {state[KEY_PROMPT_NUMBER]} of {TOT_PROMPTS_TO_DO}')
+st.progress(state[KEY_PROMPT_NUMBER]/TOT_PROMPTS_TO_DO)
+
+
+# st.title("")
 st.subheader("Story Features")
-st.write("Please use the following randomly selected features as the seed for a thumbnail image idea.")
 
-if state[KEY_CLUE] == EMPTY:
-    state[KEY_CLUE] = generate_clues()
+if state[KEY_CATEGORY] == EMPTY:
+    state[KEY_CATEGORY], state[KEY_FEATURES] = generate_clues()
 
-c1, c2 = st.columns(2)
+st.markdown(f'Please write a prompt to generate a thumbnail image for the **{state[KEY_CATEGORY]}** section using the '
+            f'randomly selected clues below.\n\nHere are some examples for inspiration:')
+
+ex = '\n\n'.join(['>'+example for example in categories_examples[state[KEY_CATEGORY]]])
+st.markdown(ex)
+
+st.header('')
+
+c1, c2 = st.columns([1, 2])
 
 with c1:
-    for category, item in state[KEY_CLUE].items():
-        st.metric(label=category, value=item)
+    st.metric(label='Newspaper Section', value=state[KEY_CATEGORY])
 with c2:
-    st.title('')
-    st.title('')
-    st.title('')
-    st.title('')
-    if state[KEY_PROMPT] == EMPTY:
-        st.button('New clues, please.', on_click=next_prompt)
+    for feature_type, feature_value in state[KEY_FEATURES].items():
+        st.metric(label=feature_type, value=feature_value)
+if state[KEY_PROMPT] == EMPTY:
+    st.button('New clues, please.', on_click=next_prompt)
 
 st.markdown('---')
-st.subheader("The prompt")
-st.write('Turn your idea into a prompt for an image generation model.  Feel free to embellish!  e.g. "Engineer" + '
-         '"Old Shoe" might become "A portrait of an engineer comparing his futuristic boot invention to an old shoe."')
-
 if state[KEY_USER_ID] != EMPTY:
     st.text_input('Enter your prompt:', key=KEY_PROMPT, on_change=reset_results)
 
@@ -201,9 +257,9 @@ with col1:
 with col2:
     st.select_slider('How happy are you with the result? (5 is "totally")', key=KEY_FEEDBACK_QUALITY, options=[1, 2, 3, 4, 5])
 
-    st.radio('Did the model include all of the aspects of your prompt?', key=KEY_FEEDBACK_FIDELITY, options=['No', 'Yes'])
+    st.radio('Was the model missing any aspects of your prompt?', key=KEY_FEEDBACK_FIDELITY, options=['No', 'Yes'])
 
-    st.radio('Did the model include any visual distortions? (e.g. crazy-fingers)', key=KEY_FEEDBACK_DISTORTION,
+    st.radio('Did the model include any visual distortions? (e.g. AI crazy-fingers)', key=KEY_FEEDBACK_DISTORTION,
                         options=['No', 'Yes'])
 
     st.radio('Did the model make any assumptions that could indicate bias? (e.g. race, gender, age)',
---
+++ pages/2 Analyzing Our Results.py
@@ -0,0 +1,206 @@
+import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
+from streamlit_plotly_events import plotly_events
+import pandas as pd
+import numpy as np
+import umap
+import boto3
+
+AWS_S3_BUCKET_NAME = st.secrets['AWS_S3_BUCKET_NAME']
+AWS_DYNAMODB_TABLE_NAME = st.secrets['AWS_DYNAMODB_TABLE_NAME']
+AWS_REGION = st.secrets['AWS_REGION']
+
+AWS_ACCESS_KEY_ID = st.secrets['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = st.secrets['AWS_SECRET_ACCESS_KEY']
+
+ddb_table = boto3.resource("dynamodb",
+                           region_name=AWS_REGION,
+                           aws_access_key_id=AWS_ACCESS_KEY_ID,
+                           aws_secret_access_key=AWS_SECRET_ACCESS_KEY).Table(
+    AWS_DYNAMODB_TABLE_NAME)
+
+s3_bucket = boto3.resource('s3',
+                           region_name=AWS_REGION,
+                           aws_access_key_id=AWS_ACCESS_KEY_ID,
+                           aws_secret_access_key=AWS_SECRET_ACCESS_KEY).Bucket(AWS_S3_BUCKET_NAME)
+
+def transform(x):
+    x['embedding'] = [float(xx) for xx in x['embedding']]
+    return x
+
+
+@st.cache_data(ttl=30)
+def get_db_data():
+    data = [transform(x) for x in ddb_table.scan()['Items']]
+    return pd.DataFrame(data)
+
+@st.cache_data
+def run_umap(the_df):
+    embs = np.asarray(the_df['embedding'].to_list())
+
+    reducer = umap.UMAP(n_components=2, n_neighbors=4, random_state=42)
+
+    umap_embs = reducer.fit_transform(embs[:, :16])
+
+    return pd.DataFrame({'UMAP_0': umap_embs[:, 0],
+                         'UMAP_1': umap_embs[:, 1],
+                         # 'UMAP_2': umap_embs[:, 2]
+                         })
+
+
+def get_data():
+    raw_df = get_db_data()
+    umap_df = run_umap(raw_df)
+    df = raw_df.copy()
+    return pd.concat([df, umap_df], axis=1)
+
+
+st.header("Part 2: Evaluating out Data and Feedback")
+
+df = get_data().copy()
+
+c1, c2 = st.columns(2)
+
+with c1:
+    sessions = df['session_id'].unique().tolist()
+    session = st.selectbox('Filter by session', ['All'] + sessions, index=len(sessions))
+
+    users = df['user'].unique().tolist()
+    user = st.selectbox('Filter by user? Optional e.g. to find your own.', ['All'] + users)
+
+with c2:
+    feedback_types = [x.split('_')[1] for x in df.columns if 'feedback' in x]
+    feedback = st.selectbox('Feedback type to color UMAP/semantic plot.', feedback_types, index = feedback_types.index('quality'))
+
+st.subheader('Click a point on plot to retrieve record.')
+
+if user == 'All':
+    d = df
+else:
+    d = df[df['user'] == user]
+
+if session != 'All':
+    d = d[d['session_id'] == session]
+
+st.write(len(d))
+
+st.title('')
+
+
+# e = np.array(d['umap_embs_0'].to_list())
+
+cc = d['feedback_' + feedback].values
+
+if type(cc[0]) is str:
+    cc = [1 if x == "Yes" else 0 for x in cc]
+
+sort_col = 'feedback_' + feedback
+
+vals = sorted(d[sort_col].unique(), reverse=True)
+
+cols = []
+for i, val in enumerate(vals):
+
+    frac_i = (len(vals)-i-1)/len(vals)
+    cols.append('#%02x%02x%02x' % (round(225*frac_i), round(60*frac_i), round(215*frac_i)))
+
+#print(col)
+
+# fig = go.Figure()
+#
+
+#
+# fig.add_trace(go.Scatter(
+#                 x=d['UMAP_0'].values,
+#                 y=d['UMAP_1'].values,
+#                 # z=d['UMAP_2'].values,
+#                 mode='markers',
+#                 # name=str(val),
+#                 marker=dict(
+#                     size=9,
+#                     color=cc,
+#                     opacity=0.8,
+#                     colorscale='Viridis',  # choose a colorscale
+#                 ),
+#              ))
+
+
+
+# d.sort_values(sort_col, inplace=True)
+
+# fig = px.scatter_3d(
+#     data_frame=d,
+#     x="UMAP_0",
+#     y="UMAP_1",
+#     z="UMAP_2",
+#     color="feedback_quality",
+#     # color_discrete_sequence=cols
+#     color_discrete_sequence=['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'],
+# )
+fig = px.scatter(
+    data_frame=d,
+    x="UMAP_0",
+    y="UMAP_1",
+    color=sort_col,
+    # color_discrete_sequence=cols
+    color_discrete_sequence=['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'],
+)
+
+# vals = sorted(d['feedback_' + feedback].unique(), reverse=True)
+# for i, val in enumerate(vals):
+#     dd = d[d['feedback_' + feedback] == val]
+#
+#
+#     frac_i = (len(vals)-i-1)/len(vals)
+#
+#     print(frac_i)
+#
+#     col = '#%02x%02x%02x' % (round(225*frac_i), round(60*frac_i), round(215*frac_i))
+#
+#     print(col)
+#
+#     fig.add_trace(go.Scatter3d(
+#                     x=dd['UMAP_0'].values,
+#                     y=dd['UMAP_1'].values,
+#                     z=dd['UMAP_2'].values,
+#                     mode='markers',
+#                     name=str(val),
+#                     marker=dict(
+#                         size=9,
+#                         color=[col for _ in range(len(dd))],
+#                         opacity=0.8,
+#                     ),
+#                  ))
+
+
+# fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
+
+fig.update_layout({"uirevision": "foo"}, overwrite=True)
+
+selected_points = plotly_events(fig, click_event=True, select_event=False)
+st.markdown('*Double-click plot to reset range and selected points.*')
+
+st.title('')
+st.title('')
+
+col_list = [None] * len(selected_points)
+
+c3, c4 = st.columns(2)
+if len(selected_points):
+
+    row_ids = [d[d['UMAP_0'] == p['x']].index.tolist()[0] for p in selected_points]
+
+    for i, row_id in enumerate(row_ids):
+
+        col_list[i] = st.columns(2)
+
+        with col_list[i][0]:
+            file_name = d.loc[row_id]['prompt_id'] + '.png'
+            s3_bucket.download_file(file_name, './' + file_name)
+            st.image('./' + file_name)
+
+        with col_list[i][1]:
+            st.write(d.loc[row_id])
+
+        break

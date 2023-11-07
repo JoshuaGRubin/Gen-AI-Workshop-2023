939f2f55d8db75b6aecd2c6d3ec5843d1a4206cb
Joshua Rubin <jgrubin@gmail.com>
2023-11-06 17:45:19 -0800

    Update link for Fiddler Summit + pin openai

--- Welcome!.py
+++ Welcome!.py
@@ -11,7 +11,7 @@ st.markdown('Find out more about us and building trust into AI at [Fiddler AI](h
 
 
 st.subheader('Additional workshop assets')
-st.markdown('- [Slides](https://docs.google.com/presentation/d/1iu5KIHMYzYtdiZOJnFdbKQEydSJUEoSZDO8KYqAYTis/edit?usp=sharing)\n\n'
+st.markdown('- [Slides](https://docs.google.com/presentation/d/1k8tnREh42G4Krm1yjSWB9NCRo0Go-BT0e21JB2FH3Ms/edit?usp=sharing)\n\n'
             '- [Colab companion notebook](https://colab.research.google.com/github/JoshuaGRubin/Gen-AI-Workshop-2023/blob/main/CompanionNotebook.ipynb)\n\n'
             '- Blog series on monitoring drift in vector distributions ([1. technique](https://www.fiddler.ai/blog/monitoring-natural-language-processing-and-computer-vision-models-part-1), [2. Computer Vision](https://www.fiddler.ai/blog/monitoring-natural-language-processing-and-computer-vision-models-part-2), [3. Natural Language](https://www.fiddler.ai/blog/monitoring-natural-language-processing-and-computer-vision-models-part-3))')
 
